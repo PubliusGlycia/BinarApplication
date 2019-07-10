168816a3ceffb7e0115b5b439fb76dd415fe499b
By698 <bysiek1998@gmail.com>
2019-07-10 14:05:11 +0200

    Added breakdowns and request lists. Added notification components.

---
+++ app/javascript/packs/notification.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default class Notification extends React.Component {
+    importanceCheck() {
+        if (this.props.importance == 'low') 
+            return '!'
+        else if (this.props.importance == 'medium')
+            return '!!'
+        else if (this.props.importance == 'high')
+            return '!!!'
+    }
+
+    render() {
+        return (
+            <>
+                <a className={this.props.isConfirmed ? 'list-group-item list-group-item-action list-group-item-success' : 'list-group-item list-group-item-action'}>
+                    <div className='row'>
+                        <h5 className='col-11'>{this.props.title}</h5>
+                        <h1 className='col-1'>{this.importanceCheck()}</h1>
+                    </div>
+                    
+                </a>
+            </>
+        )
+    }
+}
--- app/javascript/packs/notifications_list.jsx
+++ app/javascript/packs/notifications_list.jsx
@@ -1,30 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Notification from './notification';
 
 class NotificationList extends React.Component {
+    state = {
+        breakdowns: [{
+            title: 'Ekspres do kawy nie działa!',
+            importance: 'high',
+            isConfirmed: true
+        },
+        {
+            title: 'Zapchana męska toaleta',
+            importance: 'high',
+            isConfirmed: false
+        },
+        {
+            title: 'Drzwi skrzypią',
+            importance: 'medium',
+            isConfirmed: true
+        },
+        {
+            title: 'Coś nie działa',
+            importance: 'low',
+            isConfirmed: true
+        },],
+        requests: [{
+            title: 'Zielona herbata',
+            importance: 'high',
+            isConfirmed: true
+        },
+        {
+            title: 'Papier toaletowy!!!',
+            importance: 'high',
+            isConfirmed: true
+        },
+        {
+            title: 'Kawa się kończy',
+            importance: 'medium',
+            isConfirmed: false
+        },
+        {
+            title: 'W sumie to nic',
+            importance: 'low',
+            isConfirmed: false
+        },]
+    }
     render() {
+        const breakdowns = this.state.breakdowns.map(breakdown =>
+            <Notification title={breakdown.title} importance={breakdown.importance} isConfirmed={breakdown.isConfirmed}/>)
+        
+        const requests = this.state.requests.map(request =>
+            <Notification title={request.title} importance={request.importance} isConfirmed={request.isConfirmed}/>)
         return (
             <>
-                <div className='container'>
+                <div className='container-fluid'>
                     <div className='row'>
-                        <div className='col-6'>
-                            <ul className='list-group'>
-                                <li className='list-group-item'>Awaria 1</li>
-                                <li className='list-group-item'>Awaria 2</li>
-                                <li className='list-group-item'>Awaria 3</li>
-                                <li className='list-group-item'>Awaria 4</li>
-                                <li className='list-group-item'>Awaria 5</li>
-                            </ul>
+                        <div className='col'>
+
+                            <div className='list-group'>
+                                <div className='list-group-item list-group-item-secondary'><h1>Awarie</h1></div> 
+                                {breakdowns}
+                            </div>
                         </div>
                             
-                        <div className='col-6'>
-                            <ul className='list-group'>
-                                <li className='list-group-item'>Zapotrzebowanie 1</li>
-                                <li className='list-group-item'>Zapotrzebowanie 2</li>
-                                <li className='list-group-item'>Zapotrzebowanie 3</li>
-                                <li className='list-group-item'>Zapotrzebowanie 4</li>
-                                <li className='list-group-item'>Zapotrzebowanie 5</li>
-                            </ul>
+                        <div className='col'>
+                            <div className='list-group'>
+                                <div className='list-group-item list-group-item-secondary'><h1>Zapotrzebowanie</h1></div> 
+                                {requests}
+                            </div>
                         </div>
                     </div>
                 </div>  
