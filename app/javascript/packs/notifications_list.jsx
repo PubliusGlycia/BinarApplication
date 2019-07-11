d68d403c40e4ba0d779d19c17b334190bab4ec64
By698 <bysiek1998@gmail.com>
2019-07-11 14:21:03 +0200

    modal example deleted

--- app/javascript/packs/notifications_list.jsx
+++ app/javascript/packs/notifications_list.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import Notification from './notification';
 import axios from 'axios-on-rails'
 
-import { Button } from 'react-bootstrap';
-import { Modal } from 'react-bootstrap';
-
 class NotificationList extends React.Component {
     state = {
         defects: [],
@@ -74,51 +71,6 @@ class NotificationList extends React.Component {
     }
 }
 
-class Example extends React.Component {
-    constructor(props, context) {
-      super(props, context);
-  
-      this.handleShow = this.handleShow.bind(this);
-      this.handleClose = this.handleClose.bind(this);
-  
-      this.state = {
-        show: false,
-      };
-    }
-  
-    handleClose() {
-      this.setState({ show: false });
-    }
-  
-    handleShow() {
-      this.setState({ show: true });
-    }
-  
-    render() {
-      return (
-        <>
-          <Button variant="primary" onClick={this.handleShow}>
-            Launch demo modal
-          </Button>
-  
-          <Modal show={this.state.show} onHide={this.handleClose}>
-            <Modal.Header closeButton>
-              <Modal.Title>Modal heading</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={this.handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={this.handleClose}>
-                Save Changes
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </>
-      );
-    }
-  }
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
       < Example />,
