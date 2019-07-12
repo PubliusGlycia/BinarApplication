4f2c3b051326a1050462bfcbcfbcaaa942f1d80c
Adam Jędrzejec <adamjedrzejec@gmail.com>
2019-07-12 16:59:19 +0200

    równo rozłożone przyciski na pasku nawigacyjnym

--- app/javascript/packs/navbar.jsx
+++ app/javascript/packs/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CreateForm from './create_form';
+import { Container, Row, Col } from 'react-bootstrap';
 
 export default class Navbar extends React.Component {
 
@@ -36,12 +37,14 @@ export default class Navbar extends React.Component {
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <CreateForm className="nav-item" fetchPostEvents={this.props.fetchPostEvents}/>
-                        <a className="nav-item nav-link active" href="#">Zgłoszenia</a>
-                        { this.props.admin && <a className="nav-item nav-link" href="#">Archiwum</a> }
-                        { this.props.admin && <a className="nav-item nav-link" href="#">Ewidencja</a> }
-                        <a className="nav-item nav-link" href="#">Ustawienia</a>
+                    <div className="navbar-nav w-100">
+                        <Row className="w-100">
+                            <Col className="text-center"><CreateForm className="nav-item text-center" fetchPostEvents={this.props.fetchPostEvents}/></Col>
+                            <Col><a className="nav-item nav-link active text-center" href="#">Zgłoszenia</a></Col>
+                            { this.props.admin && <Col><a className="nav-item nav-link text-center" href="#">Archiwum</a></Col> }
+                            { this.props.admin && <Col><a className="nav-item nav-link text-center" href="#">Ewidencja</a></Col> }
+                            <Col><a className="nav-item nav-link text-center" href="#">Ustawienia</a></Col>
+                        </Row>
                     </div>
                 </div>
             </nav>
