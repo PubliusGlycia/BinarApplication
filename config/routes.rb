235ffe1af11069f4320344a1a5777097a1bb03c1
By698 <bysiek1998@gmail.com>
2019-07-22 15:43:16 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,8 @@ Rails.application.routes.draw do
   get 'post_events/event' => 'post_events#search_filter'
   get 'post_events/download/:id/:image_position' => 'post_events#download'
   get 'user/check' => 'post_events#check_user'
-
   get 'post_events/event/:category' => 'post_events#show_by_category'
-  resources :post_events do
-    resources :likes
-  end
+
+  resources :post_events
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
