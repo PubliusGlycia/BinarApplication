81bd91dbe32d38c615686cc14a9860ae66c698d3
Adam Jędrzejec <adamjedrzejec@gmail.com>
2019-07-23 19:45:24 +0200

    [add] db schema

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 2019_07_22_134938) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
