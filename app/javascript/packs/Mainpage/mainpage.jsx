1ad77b46492d5da70a1c475eba443a430812727d
PubliusGlycia <209415@edu.p.lodz.pl>
2019-07-24 16:26:47 +0200

    Creating welcome page

--- app/javascript/packs/Mainpage/mainpage.jsx
+++ app/javascript/packs/Mainpage/mainpage.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Button, Card, Col, ListGroup, Modal, Row} from 'react-bootstrap';
-import Image from 'react-bootstrap/Image';
+import {Button, Container} from 'react-bootstrap';
+import mem from 'images/meme.jpeg';
+import logo from 'images/ba.png';
 
 export default class MainPage extends React.Component {
     state = {
         logged: false,
     };
 
+    toAuth() {
+        this.setState({logged: true})
+    }
+
     render() {
         return (
         <>
-                <div className='title'>
-                    Hello!
-
-                </div>
+        <Container className='title'>
+            <div>
+                <img src={ logo }/>
+            </div>
+            <div>
+                BinarOffice
+            </div>
+            <div>
+                <Button variant='success' onClick={this.toAuth}>Zaloguj się przez Gmail</Button>
+            </div>
+            <div>
+                <img src={mem} />
+            </div>
+        </Container>
         </>
         );
     }
