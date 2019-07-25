ffa1ffe68b7508fcebf6ee7dad4893a88c5a0382
PubliusGlycia <209415@edu.p.lodz.pl>
2019-07-25 17:45:47 +0200

    Admin check setting for omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
         @user = User.from_omniauth(request.env['omniauth.auth'])
 
-        if @user.email === "mail"
+        if @user.email === "admin_mail"
             @user.admin = true;
         else
             @user.admin = false;
