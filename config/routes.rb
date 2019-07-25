763c486fd9b08f58ebf9eefc5a6ede22450e3d6e
By_6 <30812704+By698@users.noreply.github.com>
2019-07-25 09:51:35 +0200

    Revert "[add] add root in routes (#26)" (#45)
    This reverts commit 6675c20df24b8372bc9481d1c2284f9db9752498.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  root 'post_events#index'
-
   devise_for :users
   get 'post_events/event' => 'post_events#search_filter'
   get 'post_events/download/:id/:image_position' => 'post_events#download'
@@ -11,3 +9,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
