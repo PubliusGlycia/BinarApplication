1a83c91848f7019d5203a407890759fac8f1d660
PubliusGlycia <209415@edu.p.lodz.pl>
2019-07-26 16:53:42 +0200

    [Add] Admin email config

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
         @user = User.from_omniauth(request.env['omniauth.auth'])
 
-        if @user.email === "admin_mail"
+        if @user.email === "karolina@binarapps.com"
             @user.admin = true;
         else
             @user.admin = false;
