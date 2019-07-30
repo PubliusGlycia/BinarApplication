8516fb94178251c8c310b266d03ec5a54a43bfb7
Guciio <gucio332@gmail.com>
2019-07-30 10:46:44 +0200

    [Fix] Prepare notification display
    Notifications now are displaied the way it will be display when backend is implemented

---
+++ app/javascript/packs/Notifications/notification_button.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import {Button, ListGroup, OverlayTrigger, Popover} from 'react-bootstrap';
+import Notification from "./notification";
+
+export default class NotificationButton extends React.Component {
+
+    state = {
+        notificationList:['content','content2','content3',
+                          'content4','content5','content6',
+                          'content7','content8','content9','content10'],
+        showPopover: false
+    };
+
+    showNotificationList = () => {
+        //AXIOS GET NOTIFICATIONS
+    };
+
+    render(){
+        const notificationList = this.state.notificationList.map(notification =>
+            <ListGroup.Item style={{ background: '#AC9DC9' }}>
+                <Notification content={notification} />
+            </ListGroup.Item>
+        );
+
+        const popover = (
+            <Popover id="popover-basic" style={{ background: '#AC9DC9', maxHeight: 150, overflow: 'auto'}}>
+                <ListGroup variant="flush" >
+                    {notificationList}
+                </ListGroup>
+            </Popover>
+        );
+
+        const Notifications = () => (
+            <OverlayTrigger trigger="click" placement="bottom" overlay={popover}>
+                <Button className="button-notification">🔔</Button>
+            </OverlayTrigger>
+        );
+
+        return (<Notifications />)
+    }
+}
