e0fc7d4e22e2ed7a0665691c3fcdb6feb9a27403
PubliusGlycia <209415@edu.p.lodz.pl>
2019-07-30 10:01:41 +0200

    rubocop fixes

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,27 +1,27 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def google_oauth2
-        @user = User.from_omniauth(request.env['omniauth.auth'])
+  def google_oauth2
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
-        if @user.email === "karolina@binarapps.com"
-            @user.admin = true;
-        else
-            @user.admin = @user.id;
-        end
+  if @user.email === 'karolina@binarapps.com'
+    return @user.admin = true
+  else
+    return @user.admin = @user.id
+  end
 
-        if @user.persisted?
-            sign_in_and_redirect @user, event: authenticate_user!
-            set_flash_message(
-                :notice,
-                :success,
-                kind: 'Gmail'
-            ) if is_navigational_format?
-        else
-            session['devise.google_oauth2_data'] = request.env['omniauth.auth']
-            redirect_to new user_registration_url
-        end
+    if @user.persisted?
+      sign_in_and_redirect @user, event: authenticate_user!
+      set_flash_message(
+        :notice,
+        :success,
+        kind: 'Gmail'
+      ) if is_navigational_format?
+    else
+      session['devise.google_oauth2_data'] = request.env['omniauth.auth']
+      redirect_to new user_registration_url
     end
+  end
 
-    def failure
-        redirect_to root_path, alert: 'Gmail login failed'
-    end
+  def failure
+    redirect_to root_path, alert: 'Gmail login failed'
+  end
 end
