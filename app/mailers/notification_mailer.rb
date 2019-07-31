709b288093d32b07a571fcd980b282280adbbf99
Adam Jędrzejec <adamjedrzejec@gmail.com>
2019-07-31 13:54:51 +0200

    [config] mailer 1st step configuration

---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,23 @@
+class NotificationMailer < ApplicationMailer
+  default from: 'binarofficemailer@gmail.com'
+ 
+  def post_create_email(email)
+    mail to: email, subject: '[BO] New post has been created!'
+  end
+
+  def post_edit_email
+    mail(to: params[:user_email], subject: '[BO] Post has been edited!')
+  end
+
+  def post_delete_email
+    mail(to: params[:user_email], subject: '[BO] Post has been deleted!')
+  end
+
+  def post_approve_email
+    mail(to: params[:user_email], subject: '[BO] Your post has been approved!')
+  end
+
+  def comment_new_email
+    mail(to: params[:user_email], subject: '[BO] You have some new comments under your post!')
+  end
+end
