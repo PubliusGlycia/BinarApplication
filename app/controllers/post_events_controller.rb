a2e2cdb4dfce2c89816e8bfc6f5a7faa5931cd3a
By698 <bysiek1998@gmail.com>
2019-07-31 14:49:04 +0200

    [add] right order to shopping list

--- app/controllers/post_events_controller.rb
+++ app/controllers/post_events_controller.rb
@@ -7,7 +7,7 @@ class PostEventsController < ApplicationController
 
   def generate_pdf
     if current_user.admin
-      @post_events = PostEvent.where(id: params[:post_event_ids], category: 'supply')
+      @post_events = PostEvent.where(id: params[:post_event_ids], category: 'supply').order(title: :asc)
       name = 'Shopping_list_' + Date.current.to_s
 
       send_data(GeneratePdf.new(@post_events).render, filename: name, type: 'application/pdf', disposition: 'inline')
