27efddb287190e912304404431effc0f674ebe86
By698 <bysiek1998@gmail.com>
2019-07-31 10:29:23 +0200

    [add] seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,24 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # 10.times { Message.create!(author: Faker::Internet.email, content: Faker::Movies::HarryPotter.quote) }
-User.create(email: 'admin@binar.app', password: '123456', admin: true)
+admin = User.create(email: 'admin@binar.app', password: '123456', admin: true)
+10.times do
+  name = Faker::JapaneseMedia::SwordArtOnline.real_name
+  User.create(email: Faker::Internet.email(name: name), password: '123456')
+end
+
+users = User.where(admin: false)
+
+categories = ['defect', 'supply', 'others']
+importances = ['trival', 'important']
+
+users.each do
+  category = categories.sample
+  if category == 'supply'
+    title = Faker::JapaneseMedia::SwordArtOnline.item
+  elsif category == 'defect'
+    title = Faker::Games::Fallout.quote
+  else
+    title = Faker::Lorem.paragraph_by_chars(number: 25, supplemental: false)
+  end
+end
