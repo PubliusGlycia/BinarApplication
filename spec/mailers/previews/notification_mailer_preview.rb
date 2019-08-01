1236e9e1c2d8e340b03132d8340256e908bc08b6
Adam Jędrzejec <adamjedrzejec@gmail.com>
2019-08-01 16:30:03 +0200

    [cleanup] little fix

--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -16,12 +16,12 @@ class NotificationMailerPreview < ActionMailer::Preview
     NotificationMailer.post_delete_email('default@email.com')
   end
 
-    # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/post_approve_email
-    def post_approve_email
-      NotificationMailer.post_approve_email('default@email.com')
-    end
+  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/post_approve_email
+  def post_approve_email
+    NotificationMailer.post_approve_email('default@email.com')
+  end
 
-      # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/comment_new_email
+  # Preview this email at http://localhost:3000/rails/mailers/notification_mailer/comment_new_email
   def comment_new_email
     NotificationMailer.comment_new_email('default@email.com')
   end
