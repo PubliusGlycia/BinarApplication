a54543558cc27cb9c61582f19a0288b9219b3870
Guciio <gucio332@gmail.com>
2019-08-02 10:48:32 +0200

    [Fix] Notification messages fix

--- app/javascript/packs/Notifications/notification.jsx
+++ app/javascript/packs/Notifications/notification.jsx
@@ -6,11 +6,11 @@ export default class Notification extends React.Component {
         let output;
 
         switch(this.props.notification_type.toString()) {
-            case '1': return output = <i>Użytkownik: {this.props.user_email} dodał zgłoszenie [{this.props.notification_count}]</i>;
-            case '2': return output = <i>Użytkownik: {this.props.user_email} edytował zgłoszenie [{this.props.notification_count}]</i>;
-            case '3': return output = <i>Użytkownik: {this.props.user_email} usunął zgłoszenie [{this.props.notification_count}]</i>;
-            case '4': return output = <i>Oficer biura zatwierdził twoje zgłoszenie [{this.props.notification_count}]</i>;
-            case '5': return output = <i>Użytkownik: {this.props.user_email} skomentował post [{this.props.notification_count}]</i>;
+            case '1': return output = <i>Użytkownik: {this.props.user_email} dodał zgłoszenie</i>;
+            case '2': return output = <i>Post użytkownika został edytowany </i>;
+            case '3': return output = <i>Użytkownik: {this.props.user_email} usunął zgłoszenie </i>;
+            case '4': return output = <i>Oficer biura zatwierdził twoje zgłoszenie </i>;
+            case '5': return output = <i>Post użytkownika  {this.props.user_email} został skomentowany</i>;
             default:
                 return <i>Brak powiadomienia</i>;
         }
